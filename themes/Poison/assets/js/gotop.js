2ea8b4a928649b099c394517eb4751d88a09cf1f
Tomasz Wolanski <tomasz.wolanski@avanade.com>
2023-11-13 00:25:37 +0100

    attempt to fix go to top

--- themes/Poison/assets/js/gotop.js
+++ themes/Poison/assets/js/gotop.js
@@ -1,14 +1,10 @@
-console.info('go top');
-
-
 (() => {
     const top = document.getElementById('content-start')
     const isMobile = window.matchMedia("(max-width: 48em)").matches;
 
-    console.info(top);
-
     if (top && isMobile && !window.location.hash)
     {
         top.scrollIntoView(true);
+        //top.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     }
 })();
